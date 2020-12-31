ef5ef090d9de8868734ac84806a4f8737819f1cd
manju <manju@192.168.0.128>
2020-12-31 17:27:20 +0530

    BranchX committee

--- src/com/test1/c2.java
+++ src/com/test1/c2.java
@@ -1,10 +1,9 @@
 package com.test1;
 
-public class c1 {
+public class c2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+        System.out.println("yes");
 	}
 
 }
